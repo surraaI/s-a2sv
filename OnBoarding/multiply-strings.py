1baebbc93c2da762e75a7b8ba107d4dff42d80bb
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-12-04 21:32:47 +0300

    Added solution for Multiply Strings

--- OnBoarding/multiply-strings.py
+++ OnBoarding/multiply-strings.py
@@ -1,3 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        a = 0
+        b = 0
+        count = -1
+        for indd, d in enumerate(num1):
+            while str(count) != d:
+                count += 1
+            a += count*10**(len(num1)-1-indd)
+            count = -1
+        for indz, z in enumerate(num2):
+            while str(count) != z:
+                count += 1
+            b += count*10**(len(num2)-1-indz)
+            count = -1
+        return str(a*b)
