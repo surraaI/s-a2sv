48ef8c99d5dd77890730fea66f7abd6a987cc91c
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-01-28 23:41:58 +0300

    Add solution for Permutation in String

---
+++ weekBeforeCamp/leetcode/permutation-in-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+   
+        cntr, w = Counter(s1), len(s1)   
+
+        for i in range(len(s2)):
+            if s2[i] in cntr: 
+                cntr[s2[i]] -= 1
+            if i >= w and s2[i-w] in cntr: 
+                cntr[s2[i-w]] += 1
+
+            if all([cntr[i] == 0 for i in cntr]): # see optimized code below
+                return True
+
+        return False
