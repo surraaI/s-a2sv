e17b71ce68f18d2fe1803155cf52752f98d8453b
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-02-17 20:47:44 +0300

    Add solution for Longest Palindrome

---
+++ weekBeforeCamp/leetcode/longest-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        odd_count = 0
+        d = {}
+        for ch in s:
+            if ch in d:
+                d[ch] += 1
+            else:
+                d[ch] = 1
+            if d[ch] % 2 == 1:
+                odd_count += 1
+            else:
+                odd_count -= 1
+        if odd_count > 1:
+            return len(s) - odd_count + 1
+        return len(s)
