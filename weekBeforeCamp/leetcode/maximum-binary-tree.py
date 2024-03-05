077fb0cbff830ed630ffa694e29c4a0287053d9d
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-03-05 21:11:36 +0300

    Add solution for Maximum Binary Tree

--- weekBeforeCamp/leetcode/maximum-binary-tree.py
+++ weekBeforeCamp/leetcode/maximum-binary-tree.py
@@ -6,25 +6,14 @@
 #         self.right = right
 class Solution(object):
     def constructMaximumBinaryTree(self, nums):
-        m=max(nums)
-        i=nums.index(m)
-        root=TreeNode(m)
-        def tree(temp,left,right):
-            if left:
-                ml=max(left)
-                varl=TreeNode(ml)
-                i=left.index(ml)
-                temp.left=varl
-                if i+1==len(left): tree(temp.left,left[:i],[])
-                else: tree(temp.left,left[:i],left[i+1:])
-            if right:
-                mr=max(right)
-                varr=TreeNode(mr)
-                i=right.index(mr)
-                temp.right=varr
-                if i+1==len(right): tree(temp.right,right[:i],[])
-                else: tree(temp.right,right[:i],right[i+1:])
-        if i+1==len(nums):
-            tree(root,nums[:i],[])
-        else: tree(root,nums[:i],nums[i+1:])
+        if nums:
+            m = max(nums)
+            i = nums.index(m)
+        else:
+            return
+        root = TreeNode(m)
+        if not root:
+            return
+        root.left = self.constructMaximumBinaryTree(nums[:i])
+        root.right = self.constructMaximumBinaryTree(nums[i+1:])
         return root
