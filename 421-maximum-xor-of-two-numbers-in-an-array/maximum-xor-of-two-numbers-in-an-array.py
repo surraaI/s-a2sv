06f72fa5a22c9d89b1c1e89f07a42e48f0f90418
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-05-30 18:24:35 +0300

    Time: 4147 ms (62.64%) | Memory: 244.1 MB (67.53%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.py
@@ -1,20 +1,45 @@
-class Solution:
-    def findMaximumXOR(self, nums):    
-        max_xor = 0
-        mask = 0
-        
-        for i in range(31, -1, -1): 
-            mask |= (1 << i)
-            found_prefixes = set()
-            
-            for num in nums:
-                found_prefixes.add(num & mask)
-            
-            temp = max_xor | (1 << i)
-            for prefix in found_prefixes:
-                if (prefix ^ temp) in found_prefixes:
-                    max_xor = temp
-                    break
-        
-        return max_xor
+class Trie:
+    def __init__(self):
+        self.data = [None, None]
+    
 
+    def add(self, val):
+        node = self.data
+        for  i  in range(31,-1,-1):
+            bit = val >> i & 1
+
+            if node[bit]  is None:
+                node[bit] = [None, None] 
+            node = node[bit]
+        
+        
+    def maxXor(self, val):
+        node = self.data
+        res  = 0
+        for  i  in range(31,-1,-1):
+            bit = val >> i & 1
+            if node[not bit]  is not None:
+                node = node[not bit]
+                res |= 1 << i
+            else:
+                node = node[bit]
+            
+
+        return res
+            
+
+
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        ans =  0 
+        trie = Trie()
+
+        trie.add(nums[0])
+        for i, num in enumerate(nums):
+            if i > 0:
+                ans = max(ans, trie.maxXor(num))
+                trie.add(num)           
+
+        return ans
+        
