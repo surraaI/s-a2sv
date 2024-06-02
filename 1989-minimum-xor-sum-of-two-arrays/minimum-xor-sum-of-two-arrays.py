99118750d961b99385bf1f17f9a31cb57ef3a8cd
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-06-02 21:31:54 +0300

    Time: 297 ms (78.49%) | Memory: 27.2 MB (57.56%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.py
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.py
@@ -4,7 +4,17 @@ class Solution:
         def dp(i, used):
             if i >= n:
                 return 0
-            return min(((v ^ nums1[i]) + dp(i+1, used | (1 << j))) 
-                        for j, v in enumerate(nums2) if (1 << j) & used == 0)
-        n = len(nums2)
-        return dp(0, 0)    
+            
+            xor_sums = []
+            for j, num in enumerate(nums2):
+               
+                if (1 << j) & used == 0:
+                   
+                    xor_sum = (num ^ nums1[i]) + dp(i + 1, used | (1 << j))
+                    xor_sums.append(xor_sum)
+            
+            
+            return min(xor_sums)
+        
+        n = len(nums2)  
+        return dp(0, 0)  
