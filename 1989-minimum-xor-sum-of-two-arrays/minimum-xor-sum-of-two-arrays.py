72c36c2d4822c22c38f2a7598dc2076b0e48cfcb
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-08-20 22:51:20 +0300

    Time: 317 ms (48.61%) | Memory: 27.2 MB (50.00%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.py
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.py
@@ -1,20 +1,18 @@
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
-        @cache
-        def dp(i, used):
-            if i >= n:
+        n = len(nums1)
+        inf = 10 ** 10
+
+        @lru_cache(None)
+        def dp(i, mask):
+            if i == n:
                 return 0
             
-            xor_sums = []
-            for j, num in enumerate(nums2):
-               
-                if (1 << j) & used == 0:
-                   
-                    xor_sum = (num ^ nums1[i]) + dp(i + 1, used | (1 << j))
-                    xor_sums.append(xor_sum)
-            
-            
-            return min(xor_sums)
-        
-        n = len(nums2)  
-        return dp(0, 0)  
+            best = inf
+            for j in range(n):
+                if (mask & (1 << j)) == 0:
+                    best = min(best, dp(i+1, mask | (1 << j)) + (nums2[j] ^ nums1[i]))
+
+            return best
+
+        return dp(0,0)
