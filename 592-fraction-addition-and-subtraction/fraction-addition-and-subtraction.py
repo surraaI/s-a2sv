989d9234c1cd14df53b02759f1ae89c939bf5c72
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-09-25 22:56:47 +0300

    Time: 36 ms (54.34%) | Memory: 16.5 MB (83.93%) - LeetSync

--- 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
@@ -7,38 +7,35 @@ class Solution:
             numerator, denominator = map(int, s.split('/'))
             return numerator, denominator
 
-        # List to store the fractions
+        
         fractions = []
         num = ''
         
-        # Parse the expression into fractions
         for char in expression:
             if char in '+-':
-                if num:  # If num is not empty, append it to the fractions list
+                if num:  
                     fractions.append(num)
-                num = char  # Start a new fraction
+                num = char  
             else:
                 num += char
         
-        # Append the last fraction
+        
         fractions.append(num)
         
-        # Initialize the numerator and denominator of the result fraction
         numerator, denominator = 0, 1
         
-        # Perform the addition/subtraction
+        
         for fraction in fractions:
             frac_num, frac_den = parse_fraction(fraction)
             common_den = lcm(denominator, frac_den)
             numerator = numerator * (common_den // denominator) + frac_num * (common_den // frac_den)
             denominator = common_den
         
-        # Simplify the fraction by dividing by the GCD
         common_divisor = gcd(abs(numerator), denominator)
         numerator //= common_divisor
         denominator //= common_divisor
 
-        # If the denominator is 1, return the result as `numerator/1`
+        
         if denominator == 1:
             return f"{numerator}/1"
         
