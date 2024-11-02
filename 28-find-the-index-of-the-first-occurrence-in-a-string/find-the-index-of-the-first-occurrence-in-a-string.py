862e96a5af624eeaead8e9c9cba05823a63d4fdd
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-11-02 09:00:11 +0300

    Time: 2 ms (11.75%) | Memory: 16.7 MB (15.30%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,36 +1,55 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        
         base = 27
         MOD = 10**9 + 7
-        n, k = len(haystack), len(needle)
+        n = len(needle)
+        m = len(haystack)
+
+        if m < n:
+            return -1
+        
+        def convert(char):
+            return ord(char) - 96
+
+            
+        def add_last(Hash, char):
+            return  (Hash * base + convert(char)) % MOD
+
+
+        def poll_last(Hash, char, base_power):
+            return (Hash - convert(char) * base_power) % MOD
+
+        base_powers = [1] * (n + 1)
+        for i in range(1, n+1):
+            base_powers[i] = (base_powers[i - 1] * base) % MOD
+
+
+        target = window_hash = 0
+        for i in range(n):
+            target = add_last(target, needle[i])
+        
+        for i in range(n):
+            window_hash = add_last(window_hash, haystack[i])
+        
+        if target == window_hash:
+            if haystack[:n] == needle:
+                return 0
+        
+
+        for right in range(n, m):
+            left = right - n
+            window_hash = add_last(window_hash, haystack[right])
+            window_hash = poll_last(window_hash, haystack[left], base_powers[n])
+
+            if window_hash == target:
+                if haystack[left+1:right+1] == needle:
+                    return right - n + 1
 
         
-        if k == 0:
-            return 0
-
-        if k > n:
-            return -1
-
-        needle_hash = 0
-        window_hash = 0
-        base_pow = 1  
-
-        for i in range(k):
-            needle_hash = (needle_hash * base + (ord(needle[i]) + 1)) % MOD
-            window_hash = (window_hash * base + (ord(haystack[i]) + 1)) % MOD
-            if i < k - 1:
-                base_pow = (base_pow * base) % MOD
-
-        if needle_hash == window_hash:
-            return 0
-
-        for i in range(k, n):
-            window_hash = (window_hash - (ord(haystack[i - k]) + 1) * base_pow) % MOD
-            window_hash = (window_hash * base + (ord(haystack[i]) + 1)) % MOD
-
-            if window_hash == needle_hash:
-                if haystack[i - k + 1:i + 1] == needle:
-                    return i - k + 1
-
         return -1
+
+
+
+
+        
+       
